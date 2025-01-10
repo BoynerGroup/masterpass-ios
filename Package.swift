9a05de17f474d10cd940d32a4e4e156f2ca26903
Ata Etgi <ata.etgi@boyner.com.tr>
2025-01-10 14:39:32 +0300

    fix unstable-version package issue

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/OpenSSL-Package.git", exact: "3.3.2000"),
-        .package(url: "https://github.com/BoynerGroup/jsonmodel", branch: "master"),
+        .package(url: "https://github.com/BoynerGroup/jsonmodel", exact: "1.8.1"),
         .package(url: "https://github.com/AFNetworking/AFNetworking", exact: "4.0.1"),
     ],
     targets: [
